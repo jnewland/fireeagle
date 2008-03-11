06900b66717fec249a34db7bfbadb4b4369b308b
Kamal Fariz Mahyuddin <kamal.fariz@gmail.com>
2008-03-11 10:37:10 +0800

    Make 'rake -T' report missing gems

--- config/requirements.rb
+++ config/requirements.rb
@@ -2,10 +2,11 @@ require 'fileutils'
 include FileUtils
 
 require 'rubygems'
-%w[rake hoe newgem rubigen].each do |req_gem|
+%w[rake hoe newgem rubigen oauth geo_ruby].each do |req_gem|
   begin
     require req_gem
   rescue LoadError
+    req_gem = 'GeoRuby' if req_gem == 'geo_ruby' # gem not the same name
     puts "This Rakefile requires the '#{req_gem}' RubyGem."
     puts "Installation: gem install #{req_gem} -y"
     exit
